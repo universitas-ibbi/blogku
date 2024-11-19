a7db0dd7cc006b066065fd4cbc5b0dfb3b0b65e5
iamgymie <6257063+gymie@users.noreply.github.com>
2024-11-19 14:50:55 +0700

    initial commit

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Blog Index</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Blog Saya</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container mt-3">
+        <div class="card mb-4">
+            <div class="card-body">
+                <h2 class="card-title">Post Title</h2>
+                <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ratione obcaecati eligendi
+                    architecto soluta, exercitationem cupiditate quas iusto, nam esse repudiandae adipisci consequatur
+                    ab maiores voluptatibus rem fugit veniam. Dolores!
+                </p>
+                <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-body">
+                <h2 class="card-title">Post Title</h2>
+                <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ratione obcaecati eligendi
+                    architecto soluta, exercitationem cupiditate quas iusto, nam esse repudiandae adipisci consequatur
+                    ab maiores voluptatibus rem fugit veniam. Dolores!
+                </p>
+                <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-body">
+                <h2 class="card-title">Post Title</h2>
+                <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ratione obcaecati eligendi
+                    architecto soluta, exercitationem cupiditate quas iusto, nam esse repudiandae adipisci consequatur
+                    ab maiores voluptatibus rem fugit veniam. Dolores!
+                </p>
+                <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-body">
+                <h2 class="card-title">Post Title</h2>
+                <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ratione obcaecati eligendi
+                    architecto soluta, exercitationem cupiditate quas iusto, nam esse repudiandae adipisci consequatur
+                    ab maiores voluptatibus rem fugit veniam. Dolores!
+                </p>
+                <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
